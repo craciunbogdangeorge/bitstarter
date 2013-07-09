7ee348c98d9282e5cd50f075270ad3eb0be5cae8
Bogdan-George Craciun <craciunbogdangeorge@yahoo.com>
2013-07-09 21:38:36 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,14 +1,16 @@
+#!/usr/bin/env node
+
 var express = require('express');
 var fs = require('fs');
 var buffer;
 
+var app = express.createServer(express.logger());
+
 fs.readFile('/home/ubuntu/bitstarter/index.html', function (err, data) {
   if (err) { throw err; }
-  buffer = data;
-  console.log(buffer);    
+  buffer = data; 
+  console.log(buffer);
 });
-
-var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send(buffer.toString());
