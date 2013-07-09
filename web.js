73d798e28a28606d0d6b3956323faa51bf9e7a9b
Bogdan-George Craciun <craciunbogdangeorge@yahoo.com>
2013-07-09 15:43:57 +0000

    Updated web.js with contents from index.html

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+var buffer = fs.readFile('/bitstarter/index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+
