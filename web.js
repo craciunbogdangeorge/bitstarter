3501725ec321897385b89bf62e2e7167aef30b42
Bogdan-George Craciun <craciunbogdangeorge@yahoo.com>
2013-07-09 18:27:40 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var fs = require('fs');
-var buffer = new Buffer(16);
+var buffer;
 
 fs.readFile('/bitstarter/index.html', function (err, data) {
   if (err) { throw err; }
-  console.log(data);
+  buffer = data;
+  console.log(buffer);    
 });
 
 var app = express.createServer(express.logger());
